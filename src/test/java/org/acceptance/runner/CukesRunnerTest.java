b104582f2f7416f6c09d92570fbd763252ef8972
Amit Kumar Chaudhary <amitchaudhary.it@gmail.com>
2017-07-16 00:02:47 +0530

    updated

--- src/test/java/org/acceptance/runner/CukesRunnerTest.java
+++ src/test/java/org/acceptance/runner/CukesRunnerTest.java
@@ -5,7 +5,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions( plugin = { "pretty", "html:target/cucumber-html-report" },
+@CucumberOptions( plugin = {"html:target/cucumber-html-report",
+        "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+        "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"},
+
 		glue = "org.acceptance.steps",
 		features = "classpath:features/tutorial.feature"
 		,tags = "@test1")
